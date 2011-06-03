62c8b3b1ff0ed77241d4fa4202b5d62a5e24d763
William Spencer <wss5s@hotmail.com>
2011-06-02 20:35:52 -0400

    added default rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scheduler::Application.config.secret_token = 'fb37109a2df1b594bee4bd0020db0fa4a1bc72ad32c245533dd2d695d46d9efcc1e896d5d55ab8606b2897865533f71b00cfef5e3328863dd89dd535e730a84c'
